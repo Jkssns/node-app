90eb0253132b1f96ca11cfe67bdb8819049f01cf
zhangjun <zhangjun@acme-ai.onaliyun.com>
2021-07-23 20:22:33 +0800

    feat：后台开启端口提示

--- back-end/app.js
+++ back-end/app.js
@@ -14,7 +14,7 @@ app.all("*",function(req,res,next){
   //允许的header类型
   res.header("Access-Control-Allow-Headers", "*");
   //跨域允许的请求方式
-  res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
+  res.header("Access-Control-Allow-Methods", "*");
   
   if (req.method.toLowerCase() == 'options')
     res.send(200);  //让options尝试请求快速结束
