38062121a47d6bf39b7606bda4a001f439f1fffd
zhangjun <zhangjun@acme-ai.onaliyun.com>
2022-02-21 12:39:04 +0800

    4

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,25 +1,25 @@
 <template>
-  <router-view />
+    <router-view />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'App',
+    name: 'App',
 })
 </script>
 
 <style>
-    * {
-        margin: 0;
-        padding: 0;
-    }
-    li {
-        list-style: none;
-    }
-    #app {
-        width: 100%;
-        height: 100%;
-    }
+* {
+    margin: 0;
+    padding: 0;
+}
+li {
+    list-style: none;
+}
+#app {
+    width: 100%;
+    height: 100%;
+}
 </style>
